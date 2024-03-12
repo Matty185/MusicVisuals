0e5d63aa339d6c53b5d604c1093e73e1223df5b5
Matty <c24173436@mytudublin.ie>
2024-03-12 23:51:53 +0000

    created simple menu, added song

--- java/src/example/MyVisual.java
+++ java/src/example/MyVisual.java
@@ -6,50 +6,79 @@ public class MyVisual extends Visual {
     WaveForm wf;
     AudioBandsVisual abv;
 
+    // Variable to manage the current state of the application (menu or visualization)
+    private int currentState;
+
     public void settings() {
         size(1024, 500);
-
-        // Use this to make fullscreen
-        // fullScreen();
-
-        // Use this to make fullscreen and use P3D for 3D graphics
-        // fullScreen(P3D, SPAN);
     }
 
     public void setup() {
         startMinim();
+        // Initially, set the state to show the menu
+        currentState = 0; // 0 for menu, 1 for visualization
 
-        // Call loadAudio to load an audio file to process
-        // loadAudio("heroplanet.mp3");
-
-        // Call this instead to read audio from the microphone
-        startListening();
+        loadAudio("Vegeta x Awaken Rare Hardstyle (AniLifts Remix).mp3");
 
         wf = new WaveForm(this);
         abv = new AudioBandsVisual(this);
     }
 
     public void keyPressed() {
-        if (key == ' ') {
-            getAudioPlayer().cue(0);
-            getAudioPlayer().play();
+        if (currentState == 0) { // When in menu
+            switch (key) {
+                case '1':
+                    // Switch to visualization
+                    currentState = 1;
+                    break;
+                // Implement other cases if needed
+            }
+        } else { // When in visualization or other states
+            switch (key) {
+                case ' ':
+                    // Toggle play/pause
+                    if (getAudioPlayer().isPlaying()) {
+                        getAudioPlayer().pause();
+                    } else {
+                        getAudioPlayer().play();
+                    }
+                    break;
+                case '0':
+                    // Return to menu and stop music
+                    currentState = 0;
+                    getAudioPlayer().pause(); // Use pause() or stop() depending on your need
+                    getAudioPlayer().cue(0); // Rewind to the start of the track
+                    break;
+                case 'r':
+                    // Add functionality for 'r' if needed
+                    break;
+            }
         }
     }
 
     public void draw() {
-        background(0);
-        try {
-            // Call this if you want to use FFT data
-            calculateFFT();
-        } catch (VisualException e) {
-            e.printStackTrace();
+        if (currentState == 0) {
+            // Display menu
+            background(50);
+            textSize(32);
+            fill(255);
+            text("Menu:", 100, 100);
+            text("1. Start Visualization", 100, 150);
+            text("Press 'Space' to pause", 100, 200);
+            text("Press 'R' to restart", 100, 250);
+            // Add other menu options if necessary
+        } else if (currentState == 1) {
+            // Visualization
+            background(0);
+            try {
+                calculateFFT();
+            } catch (VisualException e) {
+                e.printStackTrace();
+            }
+            calculateFrequencyBands();
+            calculateAverageAmplitude();
+            wf.render();
+            abv.render();
         }
-        // Call this is you want to use frequency bands
-        calculateFrequencyBands();
-
-        // Call this is you want to get the average amplitude
-        calculateAverageAmplitude();
-        wf.render();
-        abv.render();
     }
 }
